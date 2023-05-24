d38814e095dcf4d71dbbff74fc41d3b67c8c06de
Tushh <tushar2643@gmail.com>
2023-05-24 22:30:20 +0530

    deployment

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, (err, res) => {
   if (err) {
     console.log(err);
